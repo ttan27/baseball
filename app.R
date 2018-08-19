81875adfd086c6ce26c69497ce5adecb8358aafc
Timothy Tan <ttan@Timothys-MacBook-Pro.local>
2018-08-19 02:03:41 -0700

    Add Teams View

--- app.R
+++ app.R
@@ -15,37 +15,96 @@ source('functions.R')
 #Defining UI
 ui <- dashboardPage(
   dashboardHeader(title = "MLB Dashboard"),
-  dashboardSidebar(),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Front Page", tabName = "front", icon = icon("dashboard")),
+      menuItem("Teams", icon = icon("th"), tabName = "teams")
+    )
+  ),
   dashboardBody(
-    fluidRow(
-      ##title = 'AL',
-      tabBox(
-        title = "American League",
-        id = "tabset1", height = "250px",
-        width = 6,
-        tabPanel("AL East", dataTableOutput('ale')),
-        tabPanel("AL Central", dataTableOutput('alc')),
-        tabPanel("AL West", dataTableOutput('alw'))
+    
+    tabItems(
+      tabItem(tabName = "front",
+        fluidRow(
+          tabBox(
+            title = "American League Standings",
+            id = "tabset1", height = 400,
+            width = 6,
+            tabPanel("AL East", div(style = 'overflow-x: scroll', DT::dataTableOutput('ale'))),
+            tabPanel("AL Central", div(style = 'overflow-x: scroll', DT::dataTableOutput('alc'))),
+            tabPanel("AL West", div(style = 'overflow-x: scroll', DT::dataTableOutput('alw')))
+          ),
+          tabBox(
+            title = "National League Standings",
+            id = "tabset2", height = 400, width = 6,
+            tabPanel("NL East", div(style = 'overflow-x: scroll', DT::dataTableOutput('nle'))),
+            tabPanel("NL Central", div(style = 'overflow-x: scroll', DT::dataTableOutput('nlc'))),
+            tabPanel("NL West", div(style = 'overflow-x: scroll', DT::dataTableOutput('nlw')))
+          )
+        ),
+        fluidRow(
+          tabBox(
+            title = "American League Leaders",
+            id = "tabset3", height = 300,
+            width = 6,
+            tabPanel("AVG", div(style = 'overflow-x: scroll', DT::dataTableOutput('alAVG'))),
+            tabPanel("H", div(style = 'overflow-x: scroll', DT::dataTableOutput('alH'))),
+            tabPanel("HR", div(style = 'overflow-x: scroll', DT::dataTableOutput('alHR'))),
+            tabPanel("SB", div(style = 'overflow-x: scroll', DT::dataTableOutput('alSB'))),
+            tabPanel("RBI", div(style = 'overflow-x: scroll', DT::dataTableOutput('alRBI')))
+          ),
+          tabBox(
+            title = "National League Leaders",
+            id = "tabset3", height = 300,
+            width = 6,
+            tabPanel("AVG", div(style = 'overflow-x: scroll', DT::dataTableOutput('nlAVG'))),
+            tabPanel("H", div(style = 'overflow-x: scroll', DT::dataTableOutput('nlH'))),
+            tabPanel("HR", div(style = 'overflow-x: scroll', DT::dataTableOutput('nlHR'))),
+            tabPanel("SB", div(style = 'overflow-x: scroll', DT::dataTableOutput('nlSB'))),
+            tabPanel("RBI", div(style = 'overflow-x: scroll', DT::dataTableOutput('nlRBI')))
+          )
+        )
       ),
-      tabBox(
-        title = "National League",
-        id = "tabset2", height = "250px", width = 6,
-        tabPanel("NL East", dataTableOutput('nle')),
-        tabPanel("NL Central", dataTableOutput('nlc')),
-        tabPanel("NL West", dataTableOutput('nlw'))
+      tabItem(tabName = "teams",
+          fluidRow(
+            box(
+              title = "Team Select",
+              width = 12,
+              selectInput("teamSel","Select Team",choices = getTeams()[1]),
+              selectInput("yearSel","Select Year",choices = NULL),
+              actionButton("selTeam", "Go")
+              
+            ),
+            tabBox(
+              title = "Batting Leaders",
+              width = 6,
+              tabPanel("AVG", )
+            ),
+            box(
+              title = "Team Results",
+              width = 12,
+              div(style = 'overflow-x: scroll', DT::dataTableOutput('teamSched'))
+            )
+          )
       )
     )
   )
 )
 
 # Define server
-server <- function(input, output) {
-  # Not yet implemented (live refresh)
-  live_standings <- reactive({
-    ##invalidateLater(60000)    # refresh the report every 60k milliseconds (60 seconds)
-    getStandings()                # call our function from above
-  })
+server <- function(input, output, session) {
+  observeEvent(input$teamSel,{
+    updateSelectInput(session,'yearSel',
+                      choices=c(2018:getTeams()[getTeams()$`Team ID`==input$teamSel, 4]))
+    ##https://stackoverflow.com/questions/48376156/updating-a-selectinput-based-on-previous-selectinput-under-common-server-functio
+  }) 
   
+  observeEvent(input$selTeam,{
+    #output team schedule and results
+    output$teamSched <- renderDataTable(getTeamDetail(input$teamSel, input$yearSel))
+    
+  })
+
   #renaming columns
   aleS <- as.data.frame(getStandings()[1])
   names(aleS) = c('Tm', 'W', 'L', 'W.L', 'GB')
@@ -67,6 +126,21 @@ server <- function(input, output) {
   output$nle <- renderDataTable(nleS)
   output$nlc <- renderDataTable(nlcS)
   output$nlw <- renderDataTable(nlwS)
+  
+  #batting league leaders
+  output$alAVG <- renderDataTable(getLeagueLeaders('al', 2018, 'AVG'))
+  output$nlAVG <- renderDataTable(getLeagueLeaders('nl', 2018, 'AVG'))
+  output$alH <- renderDataTable(getLeagueLeaders('al', 2018, 'H'))
+  output$nlH <- renderDataTable(getLeagueLeaders('nl', 2018, 'H'))
+  output$alHR <- renderDataTable(getLeagueLeaders('al', 2018, 'HR'))
+  output$nlHR <- renderDataTable(getLeagueLeaders('nl', 2018, 'HR'))
+  output$alSB <- renderDataTable(getLeagueLeaders('al', 2018, 'SB'))
+  output$nlSB <- renderDataTable(getLeagueLeaders('nl', 2018, 'SB'))
+  output$alRBI <- renderDataTable(getLeagueLeaders('al', 2018, 'RBI'))
+  output$nlRBI <- renderDataTable(getLeagueLeaders('nl', 2018, 'RBI'))
+  
+  
+  
 }
 
 # Run the application 
--- functions.R
+++ functions.R
@@ -3,6 +3,27 @@ library(tidyverse)
 library(shinydashboard)
 library(rvest)
 library(XML)
+library(stringr)
+
+library(devtools)
+library(baseballr)
+
+## Team List
+header.true <- function(df) {
+  names(df) <- as.character(unlist(df[1,]))
+  df[-1,]
+} ## from https://stackoverflow.com/questions/32054368/use-first-row-data-as-column-names-in-r
+
+getTeams <- function(){
+  teams <- html_table(read_html("https://www.baseball-reference.com/about/team_IDs.shtml"))
+  teams <- as.data.frame(teams[1])
+  teams <- header.true(teams)
+  # making present teams only
+  teams <- filter(teams, `Last Year` == 'Present')
+  return(teams)
+}
+
+
 
 ## Get Standings for home page
 getStandings <- function(){
@@ -11,7 +32,58 @@ getStandings <- function(){
   return(standings)
 }
 
-getALLeaders <- function(){
+getTeamDetail <- function(team, year){
+  return(team_results_bref(team, year))
 }
 
-alBatLeaders <- html_table(read_html("https://www.baseball-reference.com/leagues/AL/2018-batting-leaders.shtml"))
+
+getLeagueLeaders <- function(league, year, stat){
+  statDB <- fg_bat_leaders(year, year, league)
+  statDB <- arrange(statDB, desc(!!as.name(stat)))
+  statTable <- statDB[1:5,c('Name', 'Team', stat)]
+  return(statTable)
+}
+
+
+statDB <- fg_bat_leaders(2018, 2018, 'al')
+statDB <- arrange(statDB, desc(!!as.name('HR')))
+statDB <- filter(statDB, Team == 'Mariners')
+
+getTeamLeaders <- function(year, stat, team, qualified){
+  url <- "https://www.baseball-reference.com/teams/"
+  url <- paste(url, as.character(team), "/", as.character(year), ".shtml#all_team_batting", sep = "")
+  teamDB <- url %>%
+    read_html() %>%
+    html_nodes(xpath='//*[@id="team_batting"]') %>%
+    html_table()
+  teamDB <- cleanUpTeamLeaders(teamDB)
+  statLeader <- arrange(teamDB, desc(!!as.name(stat)))
+  statLeaderQualified <- filter(statLeader, PA >= 3.1*G)
+  if(qualified == TRUE){
+    return(statLeaderQualified[1:5, c('Name', stat)])
+  }
+  return(statLeader[1:5, c('Name', stat)])
+  
+}
+
+max(sea$G)
+
+cleanUpTeamLeaders <- function(db){
+  db <- filter(db, Rk != 'Rk')
+  db <- db[1:(nrow(db)-4),]
+  for(i in c(1,4:28)){
+    db[,i] <- as.numeric(db[,i])
+  }
+  return(db)
+}
+
+seaQ <- filter(sea, PA >= 3.1*max(G))
+
+#url <- "https://www.baseball-reference.com/leagues/AL/2018-standard-batting.shtml"
+#valuation_col <- url %>%
+ # read_html() %>%
+  #html_nodes(xpath='//*[@id="div_players_standard_batting"]') %>%
+  #html_table()
+
+
+
